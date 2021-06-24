7216e365b04252592675faba3a4cdc8ed9c5f98d
Gabriel Andrade <gabriel.andrade200p@gmail.com>
2021-06-24 19:20:20 -0300

    1.0.7 - Done Smoth Scroll
    Feito um scroll mais suave, sombra no header e transição mais plástica para o menu.

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-// ============ VARIABLES ============
+// ============ LOOPS CLICK MENU ============
 
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 const links = document.querySelectorAll('nav ul li a')
 
-
-// ============ LOOPS ============
-
-for(const element of toggle) {
+for (const element of toggle) {
   element.addEventListener('click', function() {
     nav.classList.toggle('show')
   })
@@ -18,3 +15,19 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+// ============ LOOPS HEADER SCROLL ============
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+
+  if(window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } 
+  
+  else {
+    header.classList.remove('scroll')
+  }
+})
