c247c0d2c1b4d20a1e3ff9cf92a5819c8e2c2e84
Gabriel300p <gabriel.andrade200p@gmail.com>
2021-06-24 00:08:42 -0300

    1.0.5 - Done Mobile Menu, Section Abount and Section Services
    Feito o menu mobile em JavaScript. Terminado section sobre nós e serviços, terminado o CSS e HTML das duas seções.

---
+++ main.js
@@ -0,0 +1,20 @@
+// ============ VARIABLES ============
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+const links = document.querySelectorAll('nav ul li a')
+
+
+// ============ LOOPS ============
+
+for(const element of toggle) {
+  element.addEventListener('click', function() {
+    nav.classList.toggle('show')
+  })
+}
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
